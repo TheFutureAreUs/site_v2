6c690966e7a151a2939a522466df60fc7d63228a
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-16 23:33:20 -0400

    .

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+
+  def privacy
+  end
+
+  def terms
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
+  resources :categories
+  resources :listings
+
+
+  resources :regions do
+    resources :colleges
+  end
+
+  get "/search" => "listings#search"
 
   root 'welcome#index'
+
+
+  match '/about',       to: 'pages#about',          via: :get
+  match '/privacy',     to: 'pages#privacy',        via: :get
+  match '/terms',       to: 'pages#terms',          via: :get
+  match '/mylistings',  to: 'listings#mylistings',  via: :get
 end

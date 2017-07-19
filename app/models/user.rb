ff2a722a9b09a32c8a9a46f65e1af48dbaa3ffa8
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-18 22:21:46 -0400

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,45 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
+
+  has_many :listings, dependent: :destroy
+
+  validates :email,
+    format: {
+      with: /\.edu\z/,
+      message: "only allows .edu addresses"
+    }
+
+  validates :username,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
+   # Makes sure that users can't use another persons username
+    validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  #validate :validate_username
+
+  #def validate_username
+    #if User.where(email: username).exists?
+      #errors.add(:username, :invalid)
+    #end
+  #end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", {:value => login.downcase}]).first
+    else
+      if conditions[:username].nil? 
+        where(conditions).first 
+      else 
+        where(username: conditions[:username]).first
+      end
+    end 
+  end
 end

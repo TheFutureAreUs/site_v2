6c736866551547fe39a6d9ed53d092b554828e3e
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-19 14:58:26 -0400

    adding more css templates

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,2 +1,13 @@
 class RegionsController < ApplicationController
+
+  def index
+    @regions = Region.all
+    @maryland_colleges = @regions[0]
+  end
+
+  def show
+    @listings = Listing.where(category_id: params[:id]).order("created_at DESC")
+    @category = Category.find(params[:id])
+  end
+
 end

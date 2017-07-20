87b0498e5d8a6cacff704d89cc19c711a596c2f6
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-20 12:13:28 -0400

    .

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,20 @@ class WelcomeController < ApplicationController
   def index
     @recent_listings = Listing.last(6).reverse
 		create_markers
+
+    @category = Category.all
+    @automotive = @category[0]
+    @beauty = @category[1]
+    @technology = @category[2]
+    @academic = @category[3]
+    @art = @category[4]
+    @photography = @category[5]
+    @culinary = @category[6]
+    @fashion = @category[7]
+    @fitness = @category[8]
+    @childcare = @category[9]
+    @event_services = @category[10]
+    @other = @category[11]
   end
 
   def create_markers

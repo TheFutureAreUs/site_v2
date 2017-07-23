4af8a6d032975d0580d825e2b2723d77d3b3c77a
Kwesi Larbi <kwesiiilarb@gmail.com>
2017-07-23 13:31:27 -0400

    Fixing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723161626) do
+ActiveRecord::Schema.define(version: 20170723173016) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170723161626) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.text     "body"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "regions", force: :cascade do |t|
